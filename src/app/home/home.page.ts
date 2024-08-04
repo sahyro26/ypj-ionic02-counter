d9e70d39c55405417d7e2ea0ed7adbb46e9fa6ce
sahyro <sahyro@genejunction.com>
2024-08-04 10:14:26 +0800

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  counter:number = 0
+  decrementDisabled:boolean = true;
+  message:String = ''
+
+  constructor() { }
+
+  incrementPressed() {
+    this.counter++
+    this.decrementDisabled = false
+  }
+
+  decrementPressed() {
+    if (this.counter > 0) {
+      this.counter--
+    }
+    else{
+      this.decrementDisabled = true
+      alert('Number already 0')
+      this.message='Counter already zero'
+    }
+  }
+
+  resetPressed() {
+    this.counter = 0
+    this.decrementDisabled = true
+    this.message=''
+  }
 
 }
